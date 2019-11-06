ae253af4de5b5cafdc656ae3385eb23f329c55eb
sudoStatus200 <rsmawari198@gmail.com>
2019-11-06 19:29:55 +0530

    put inside src

---
+++ string/duplicates/countDuplicates.js
@@ -0,0 +1,24 @@
+const countDuplicates = (string, toLowerCase = true) => {
+  if (!string && string === undefined) return 0;
+  try {
+    let modifiedString;
+    toLowerCase
+      ? (modifiedString = string.toLowerCase())
+      : (modifiedString = string);
+
+    const result = modifiedString.split("").reduce((acc  , curr)=>
+          acc[curr] === undefined
+          ? Object.assign(acc, { [curr]: 1 })
+          : Object.assign(acc, { [curr]: acc[curr] + 1 }),
+        {}
+      );
+    console.log(result);
+
+    return result;
+  } catch (e) {
+    return 0;
+  }
+};
+
+let f = "dddd";
+countDuplicates(f);
